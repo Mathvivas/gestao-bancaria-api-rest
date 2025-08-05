37ed6b4eecd7f64d5bae05cdef73d4b564377fe1
Mathvivas <mathvivas@hotmail.com>
2025-08-05 19:00:28 -0300

    Criação da Entidade JPA Conta

--- src/main/java/com/matheus/gestao_bancaria_api_rest/conta/Conta.java
+++ src/main/java/com/matheus/gestao_bancaria_api_rest/conta/Conta.java
@@ -1,4 +1,19 @@
 package com.matheus.gestao_bancaria_api_rest.conta;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "conta")
+@Entity(name = "Conta")
 public class Conta {
+
+    @Id
+    private int id;
+    private float saldo;
+
+    public Conta(DadosConta dados) {
+        this.id = dados.id();
+        this.saldo = dados.saldo();
+    }
 }
