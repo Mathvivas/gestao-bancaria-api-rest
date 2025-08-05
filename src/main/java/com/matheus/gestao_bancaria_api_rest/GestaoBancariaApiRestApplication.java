d5338d82abbbc86a5a37f9119fdeec3631ce7564
Mathvivas <mathvivas@hotmail.com>
2025-08-05 17:40:27 -0300

    Criação do projeto a partir do Spring Initializr, com todas as dependências que serão necessárias até o momento

---
+++ src/main/java/com/matheus/gestao_bancaria_api_rest/GestaoBancariaApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.gestao_bancaria_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoBancariaApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoBancariaApiRestApplication.class, args);
+	}
+
+}
