779cbe45aeee0439c1d64773e66acd6d35701a46
Mathvivas <mathvivas@hotmail.com>
2025-08-05 19:03:12 -0300

    Adição de anotações do Lombok para facilitar

--- src/main/java/com/matheus/gestao_bancaria_api_rest/conta/Conta.java
+++ src/main/java/com/matheus/gestao_bancaria_api_rest/conta/Conta.java
@@ -3,9 +3,17 @@ package com.matheus.gestao_bancaria_api_rest.conta;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name = "conta")
 @Entity(name = "Conta")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Conta {
 
     @Id
