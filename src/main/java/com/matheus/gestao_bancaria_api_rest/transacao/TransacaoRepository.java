7219c18525ecb849df1ab280f14c076aeeeb51f4
Mathvivas <mathvivas@hotmail.com>
2025-08-06 01:59:51 -0300

    Remoção da Interface JPA da Transação pois a mesma não possui necessidade de banco de dados

--- src/main/java/com/matheus/gestao_bancaria_api_rest/transacao/TransacaoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.matheus.gestao_bancaria_api_rest.transacao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransacaoRepository extends JpaRepository<Transacao, Integer> {
-}
